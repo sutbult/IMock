317740c2699177fc6e7c1cdfd80ba1f382c9319b
Samuel Utbult <samuel.utbult@hotmail.com>
2022-03-26 17:36:55 +0100

    Add comments to test/src/IMock.cpp

--- test/src/IMock.cpp
+++ test/src/IMock.cpp
@@ -5,19 +5,20 @@
 #include <IMock.hpp>
 
 TEST_CASE("can mock a basic interface", "[basic]") {
+    // Declare a basic interface.
     class ICalculator {
         public:
-            virtual ~ICalculator() {
-            }
             virtual int add(int, int) = 0;
             virtual int subtract(int, int) = 0;
             virtual int multiply(int, int) = 0;
             virtual int divide(int, int) = 0;
     };
 
+    // Create a Mock of ICalculator.
     IMock::Mock<ICalculator> mock;
 
     SECTION("call add when it has not been mocked") {
+        // Perform the call and verify it throws an UnknownCallException.
         REQUIRE_THROWS_MATCHES(
             mock.get().add(1, 1),
             IMock::UnknownCallException,
@@ -26,16 +27,20 @@ TEST_CASE("can mock a basic interface", "[basic]") {
     }
 
     SECTION("mock add") {
+        // Mock add.
         IMock::MockCaseCallCount mockCaseCallCount = when(mock, add)
             .with(1, 1)
             .returns(2);
 
         SECTION("no calls have initially been made") {
             SECTION("verifyNeverCalled() does not throw any exception") {
+                // Perform the call and verify it does not throw an exception.
                 REQUIRE_NOTHROW(mockCaseCallCount.verifyNeverCalled());
             }
 
             SECTION("verifyCalledOnce() throws a WrongCallCountException") {
+                // Perform the call and verify it throws a
+                // WrongCallCountException.
                 REQUIRE_THROWS_MATCHES(
                     mockCaseCallCount.verifyCalledOnce(),
                     IMock::WrongCallCountException,
@@ -45,19 +50,25 @@ TEST_CASE("can mock a basic interface", "[basic]") {
         }
 
         SECTION("call add with the mocked values") {
+            // Call add with the mocked values.
             int result = mock.get().add(1, 1);
 
             SECTION("the result is correct") {
+                // Verify the result equals 2.
                 REQUIRE(result == 2);
             }
 
             SECTION("the call count is one") {
                 SECTION("verifyCalledOnce() does not throw any exception") {
+                    // Perform the call and verify it does not throw an
+                    // exception.
                     REQUIRE_NOTHROW(mockCaseCallCount.verifyCalledOnce());
                 }
 
                 SECTION("verifyNeverCalled() throws a "
                     "WrongCallCountException") {
+                    // Perform the call and verify it throws a
+                    // WrongCallCountException.
                     REQUIRE_THROWS_MATCHES(
                         mockCaseCallCount.verifyNeverCalled(),
                         IMock::WrongCallCountException,
@@ -68,6 +79,7 @@ TEST_CASE("can mock a basic interface", "[basic]") {
         }
 
         SECTION("call add with unmocked values") {
+            // Perform the call and verify it throws an UnmockedCallException.
             REQUIRE_THROWS_MATCHES(
                 mock.get().add(1, 2),
                 IMock::UnmockedCallException,
@@ -77,6 +89,7 @@ TEST_CASE("can mock a basic interface", "[basic]") {
         }
 
         SECTION("call another method that has not been mocked") {
+            // Perform the call and verify it throws an UnknownCallException.
             REQUIRE_THROWS_MATCHES(
                 mock.get().subtract(1, 1),
                 IMock::UnknownCallException,
@@ -85,38 +98,51 @@ TEST_CASE("can mock a basic interface", "[basic]") {
         }
 
         SECTION("mock add again") {
+            // Mock add with other values.
             IMock::MockCaseCallCount mockCaseCallCountSecond = when(mock, add)
                 .with(2, 2)
                 .returns(5);
 
             SECTION("call add with the second mock") {
+                // Call add with the values of the second mock.
                 int result = mock.get().add(2, 2);
 
                 SECTION("the result is correct") {
+                    // Verify the result is 5.
                     REQUIRE(result == 5);
                 }
 
                 SECTION("the call count is one") {
+                    // Perform the call and verify it does not throw an
+                    // exception.
                     REQUIRE_NOTHROW(mockCaseCallCountSecond.verifyCalledOnce());
                 }
 
                 SECTION("the call count for the first mock is zero") {
+                    // Perform the call and verify it does not throw an
+                    // exception.
                     REQUIRE_NOTHROW(mockCaseCallCount.verifyNeverCalled());
                 }
             }
 
             SECTION("call add with the first mock") {
+                // Call add with the values of the first mock.
                 int result = mock.get().add(1, 1);
 
                 SECTION("the result is correct") {
+                    // Verify the result is 2.
                     REQUIRE(result == 2);
                 }
 
                 SECTION("the call count is one") {
+                    // Perform the call and verify it does not throw an
+                    // exception.
                     REQUIRE_NOTHROW(mockCaseCallCount.verifyCalledOnce());
                 }
 
                 SECTION("the call count for the second mock is zero") {
+                    // Perform the call and verify it does not throw an
+                    // exception.
                     REQUIRE_NOTHROW(mockCaseCallCountSecond
                         .verifyNeverCalled());
                 }
@@ -124,39 +150,52 @@ TEST_CASE("can mock a basic interface", "[basic]") {
         }
 
         SECTION("mock subtract") {
+            // Mock subtract.
             IMock::MockCaseCallCount mockCaseCallCountSecond =
                 when(mock, subtract)
                     .with(1, 1)
                     .returns(0);
 
             SECTION("call subtract") {
+                // Call subtract with the mocked values.
                 int result = mock.get().subtract(1, 1);
 
                 SECTION("the result is correct") {
+                    // Verify the result is 0.
                     REQUIRE(result == 0);
                 }
 
                 SECTION("the call count is one") {
+                    // Perform the call and verify it does not throw an
+                    // exception.
                     REQUIRE_NOTHROW(mockCaseCallCountSecond.verifyCalledOnce());
                 }
 
                 SECTION("the call count for the first mock is zero") {
+                    // Perform the call and verify it does not throw an
+                    // exception.
                     REQUIRE_NOTHROW(mockCaseCallCount.verifyNeverCalled());
                 }
             }
 
             SECTION("call add") {
+                // Call add with the mocked values.
                 int result = mock.get().add(1, 1);
 
                 SECTION("the result is correct") {
+                    // Verify the result is 2.
                     REQUIRE(result == 2);
                 }
 
                 SECTION("the call count is one") {
+                    // Perform the call and verify it does not throw an
+                    // exception.
                     REQUIRE_NOTHROW(mockCaseCallCount.verifyCalledOnce());
                 }
 
                 SECTION("the call count for the second mock is zero") {
+                    // Perform the call and verify it does not throw an
+                    // exception.
                     REQUIRE_NOTHROW(mockCaseCallCountSecond
                         .verifyNeverCalled());
                 }
@@ -165,24 +204,31 @@ TEST_CASE("can mock a basic interface", "[basic]") {
     }
 
     SECTION("mock add in a loop") {
+        // Create a vector for the MockCaseCallCount instances.
         std::vector<IMock::MockCaseCallCount> mockCaseCallCounts;
 
+        // Iterate twice.
         for(int i = 0; i < 2; i++) {
+            // Mock add.
             mockCaseCallCounts.push_back(when(mock, add)
                 .with(i, i)
                 .returns(i * 2));
         }
 
         SECTION("call add with the mocked values") {
+            // Continue with each index.
             int i = GENERATE(0, 1);
 
+            // Call add using the current index.
             int result = mock.get().add(i, i);
 
             SECTION("the result is correct") {
+                // Verify the result is i * 2.
                 REQUIRE(result == i * 2);
             }
 
             SECTION("the call count is one") {
+                // Perform the call and verify it does not throw an exception.
                 REQUIRE_NOTHROW(mockCaseCallCounts[i].verifyCalledOnce());
             }
         }
