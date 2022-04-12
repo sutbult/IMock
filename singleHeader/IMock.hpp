80f2b73588beffc6566e4b3b5bcdf024ba2bb475
Samuel Utbult <samuel.utbult@hotmail.com>
2022-04-12 19:35:07 +0200

    Update the single header for VirtualTable

--- singleHeader/IMock.hpp
+++ singleHeader/IMock.hpp
@@ -432,6 +432,35 @@ VirtualTableSize getVirtualTableSize() {
 namespace IMock::Internal {
 
 template <typename TInterface>
+class VirtualTable {
+    private:
+        VirtualTableSize _virtualTableSize;
+        std::unique_ptr<void*, std::function<void(void**)>> _virtualTable;
+
+    public:
+        VirtualTable() 
+            : _virtualTableSize(Internal::getVirtualTableSize<TInterface>())
+            , _virtualTable(
+                new void*[_virtualTableSize],
+                [](void** virtualTable) {
+                    delete[] virtualTable;
+                }) {
+            std::fill(
+                _virtualTable.get(),
+                _virtualTable.get() + _virtualTableSize,
+                reinterpret_cast<void*>(Internal::UnknownCall::onUnknownCall));
+        }
+
+        void** get() const {
+            return _virtualTable.get();
+        }
+};
+
+}
+
+namespace IMock::Internal {
+
+template <typename TInterface>
 class InnerMock {
     private:
         struct MockFake {
@@ -458,24 +487,13 @@ class InnerMock {
             std::unique_ptr<Internal::ICaseNonGeneric>
         > _cases;
 
-        Internal::VirtualTableSize _virtualTableSize;
-        std::unique_ptr<void*, std::function<void(void**)>> _virtualTable;
+        VirtualTable<TInterface> _virtualTable;
 
         MockFake _mockFake;
 
     public:
-        InnerMock() 
-            : _virtualTableSize(Internal::getVirtualTableSize<TInterface>())
-            , _virtualTable(
-                new void*[_virtualTableSize],
-                [](void** virtualTable) {
-                    delete[] virtualTable;
-                })
-            , _mockFake(_virtualTable.get(), this) {
-            std::fill(
-                _virtualTable.get(),
-                _virtualTable.get() + _virtualTableSize,
-                reinterpret_cast<void*>(Internal::UnknownCall::onUnknownCall));
+        InnerMock()
+            : _mockFake(_virtualTable.get(), this) {
         }
 
         virtual ~InnerMock() {
