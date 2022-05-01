6188d9b00ca5a598ff21a705e2753447d2c9ce6d
Samuel Utbult <samuel.utbult@hotmail.com>
2022-05-01 20:13:21 +0200

    Rename certain names in include/internal/make_unique.hpp

--- singleHeader/IMock.hpp
+++ singleHeader/IMock.hpp
@@ -109,24 +109,24 @@ namespace IMock::Internal {
 
 /// Creates a unique_ptr.
 ///
-/// @param __args Arguments used to create the contained value.
+/// @param arguments Arguments used to create the contained value.
 /// @return A created unique_ptr.
-template<typename _Tp, typename... _Args>
-std::unique_ptr<_Tp> make_unique(_Args&&... __args)
+template<typename TPointer, typename... TArguments>
+std::unique_ptr<TPointer> make_unique(TArguments&&... arguments)
 {
     // Check if std::make_unique is available (introduced in C++14).
     #ifdef __cpp_lib_make_unique
 
     // Use std::make_unique to create the unique_ptr since std::make_unique is
     // available.
-    return std::make_unique<_Tp, _Args...>(
-        std::forward<_Args>(__args)...);
+    return std::make_unique<TPointer, TArguments...>(
+        std::forward<TArguments>(arguments)...);
     #else
 
     // Create an instance manually and then create a unique_ptr using it since
     // std::make_unique is not available.
-    return std::unique_ptr<_Tp>(
-        new _Tp(std::forward<_Args>(__args)...));
+    return std::unique_ptr<TPointer>(
+        new TPointer(std::forward<TArguments>(arguments)...));
     #endif
 }
 
