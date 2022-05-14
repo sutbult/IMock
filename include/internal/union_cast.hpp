038d6474bff260e16f3e8c9286f2d211ce589f5c
Samuel Utbult <samuel.utbult@hotmail.com>
2022-05-14 15:53:40 +0200

    Move onCall to MockFake to make it a method instead of a static method

---
+++ include/internal/union_cast.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+namespace IMock {
+namespace Internal {
+
+/// Casts a value of any type to a value of any other type with regards to the
+/// raw memory using a union. Use with caution.
+/// @param source The value to cast from.
+/// @return The casted value.
+template <typename TTarget, typename TSource>
+TTarget union_cast(TSource source) {
+    // Create a union with a member each for the source and the target.
+    union
+    {
+        TSource source;
+        TTarget target;
+    } castUnion;
+
+    // Store the source in the union.
+    castUnion.source = source;
+
+    // Retrieve the target from the union and return it.
+    return castUnion.target;
+}
+
+}
+}
